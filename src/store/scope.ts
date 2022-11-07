c444de18c0f9551bdc323183f17cf37390e92c84
qiutian00 <1476102147@qq.com>
2022-11-07 21:35:46 +0800

    fix: circle import

--- src/store/scope.ts
+++ src/store/scope.ts
@@ -1,10 +1,7 @@
 import { __SCOPE_PREFIX__, __INTERNAL_SCOPE_PREFIX__ } from '../constants'
 import { isInternalKey } from '../utils'
 
-export const getScope = (scope: string) => {
-  const isInternal = isInternalKey(scope)
-  return `${isInternal ? __INTERNAL_SCOPE_PREFIX__ : __SCOPE_PREFIX__}${scope}`
-}
+export { getScope } from '../utils';
 
 export const getKeyWithScope = (scope: string, key: string) => {
   const isInternal = isInternalKey(scope)
