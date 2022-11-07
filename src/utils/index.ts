3e25fa9fe543b98cf457d98b145731e94739edcf
qiutian00 <1476102147@qq.com>
2022-11-07 21:35:46 +0800

    fix: circle import

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,15 @@
-import { __INTERNAL_KEY__ } from '../constants'
-import { getScope } from '../store/scope'
+import { __INTERNAL_KEY__, __INTERNAL_SCOPE_PREFIX__, __SCOPE_PREFIX__ } from "../constants";
 
 export const isFunction = (obj: any) => typeof obj === 'function'
 
 export const isString = (obj: any) => typeof obj === 'string'
 
 export const isInternalKey = (key: string) => ~__INTERNAL_KEY__.indexOf(key)
+
+export const getScope = (scope: string) => {
+  const isInternal = isInternalKey(scope)
+  return `${isInternal ? __INTERNAL_SCOPE_PREFIX__ : __SCOPE_PREFIX__}${scope}`
+}
 
 export const isAffiliateInternalKey = (key: string) => {
   return __INTERNAL_KEY__.some((internalKey) => {
